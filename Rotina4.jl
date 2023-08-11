a12659094a61351608323a4a1f55510ce29a0090
joaopedroj <84742010+joaopedroj@users.noreply.github.com>
2023-08-11 10:35:10 -0300

    Add files via upload

--- Rotina4.jl
+++ Rotina4.jl
@@ -6,12 +6,7 @@ function Rotina2(
     w₀::Number,
     th::Any)
 
-    # var_final = zeros(length(th))
 
-    # for i in 1:length(th)
-    #     var_final[i] = Rotina1(R, N, Δ, s, w₀, th[i])
-
-    # end
     var_final = @showprogress pmap(1:length(th)) do ind
         var = Rotina1(R, N, Δ, s, w₀, th[ind])
         var
